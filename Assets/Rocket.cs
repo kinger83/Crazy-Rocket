9bc1af0b83c4e9dda733b89650b64437ebd2832b
DESKTOP-UU1PT5B\kinge <kinger83@outlook.com>
2018-01-19 15:31:49 +1100

    Working thrust with audio and using input manager

---
+++ Assets/Rocket.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+	// Use this for initialization
+	void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        bool thrust = CrossPlatformInputManager.GetButton("Thrust");
+        if (thrust)
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            
+
+            print("Thrust = " + thrust);
+        }
+        else
+        {
+            audioSource.Stop();
+            return;
+        }
+        
+    }
+
+    private void ProcessRotation()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        if (xThrow < 0)
+        {
+            print(" Rotating Left, " + xThrow);
+        }
+        else if(xThrow > 0)
+        {
+            print("Rotating Right, " + xThrow);
+        }
+        
+    }
+}
